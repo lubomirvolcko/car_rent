ec4a98565d2f117bbe224687d817e3abdaee38d8
unknown <martin.hercka@akademiasovy.sk>
2018-02-15 17:34:58 +0100

    -updated contact page

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
  $(document).ready(function() {
  	var ba=$("#ba").val();
-
+console.log(ba);
  $("#ba").click(function(){
 var city = "Bratislava";
 var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
 urllink=urllink + city;
- urllink=urllink+'&appid=9f3132ee22da51a2a27391d8a89eb76f';
+urllink=urllink+'&appid=9f3132ee22da51a2a27391d8a89eb76f';
 console.log(urllink);
 
-   $.ajax({
-         url: urllink,
-         data : { format: 'json' } ,
-         error : function(){
-        
-         },
-         dataType: 'json',
-         success : function(data)
-         {
-  var firstcoord=data.coord.lon;
-            var secondcoord=data.coord.lat;
-            console.log(firstcoord+","+secondcoord);
   
 
-  var uluru = {lat: secondcoord, lng:firstcoord};
+  var uluru = {lat:48.15, lng:17.11};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 8,
           center: uluru
@@ -33,8 +21,66 @@ console.log(urllink);
         });
 
            
-         },
-         type: 'GET'
+         });
+
+
+  $("#bb").click(function(){
+var city = "Bratislava";
+var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
+urllink=urllink + city;
+urllink=urllink+'&appid=9f3132ee22da51a2a27391d8a89eb76f';
+console.log(urllink);
+
+  
+
+  var uluru = {lat:48.73, lng:19.14};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+           
+         });
+
+
+
+   $("#ke").click(function(){
+        var uluru = {lat:48.71, lng:21.25};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+           
+         });
+
+
+    $("#wien").click(function(){
+
+    var uluru = {lat:48.21, lng:16.37};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+           
+         });
+        
+        
+        
+        
 
 
 
@@ -52,7 +98,4 @@ console.log(urllink);
 
 
 
-
-});
-});
 });
