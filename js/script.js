ce3e2aa3c50f15940857803970e81dac570cf60b
Martinhercka <martin.hercka@akademiasovy.sk>
2018-02-21 19:07:17 +0100

    -Choose car by category&select pick/deliver place

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
  	var ba=$("#ba").val();
-console.log(ba);
+
  $("#ba").click(function(){
 var city = "Bratislava";
 var urllink='http://api.openweathermap.org/data/2.5/weather?q=';
@@ -77,10 +77,129 @@ console.log(urllink);
 
            
          });
+    
+//READ DATA FROM DATABASE-----------------------------------------------------------------------------------
+
+    $("#cat").change(function(){
+      
+      $.ajax({
+
+    url: 'http://localhost:8080/rent/car/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getCars',
+    contentType:'application/json',
+    success: getCars,
+   type:'GET'
+   
+
+
+
         
+     });   
+ 
+
+
+ function getCars(data)
+{    console.log(data);
+    var temp=data.cars;
+    
+     $("#dbcar").empty();
+     for(var i = 0;i<temp.length;i++){
+        var option = document.createElement("option");
+         var ajaxprint = document.createTextNode(temp[i]);
+         option.appendChild(ajaxprint);
+         document.getElementById("dbcar").appendChild(option);
+     }
+   
+      
+}
+
+});
+
+   
+
+ $("#pickup").change(function(){
+
+  $.ajax({
+
+    url: 'http://localhost:8080/rent/city/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getCity',
+    contentType:'application/json',
+    success: getCity,
+    type:'GET'
+   
+
+
+
+
         
+     }); 
+
+
+ 
+ function getCity(data)
+{    console.log(data);
+    
+    
+     
+    
+      
+}
+
+   }); 
+
+
+$("#deliver").change(function(){
+
+  $.ajax({
+
+    url: 'http://localhost:8080/rent/decity/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getCity',
+    contentType:'application/json',
+    success: getCity,
+    type:'GET'
+   
+
+
+
+
         
-        
+     }); 
+
+
+ 
+ function getDeCity(data)
+{    console.log(data);
+    
+    
+     
+    
+      
+}
+
+   }); 
+
+
+
+
+
 
 
 
