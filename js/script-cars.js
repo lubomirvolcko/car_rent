35f4b024d79b0c4beabfedf0d4978c149d9c8d1f
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2018-03-05 13:12:10 +0100

    update gallery 2

--- js/script-cars.js
+++ js/script-cars.js
@@ -66,6 +66,61 @@ function setTesla() {
     openModal(17);
 }
 
+function setAventador() {
+    openModal(18);
+}
+
+function setMclaren() {
+    openModal(19);
+}
+
+function setAston() {
+    openModal(20);
+}
+
+function setChiron() {
+    openModal(21);
+}
+
+function setViper() {
+    openModal(22);
+}
+
+function setNissan() {
+    openModal(23);
+}
+
+function setCamaro() {
+    openModal(24);
+}
+
+function setMustang() {
+    openModal(25);
+}
+
+function setAlfa() {
+    openModal(26);
+}
+
+function setGts() {
+    openModal(27);
+}
+
+function setCorvette() {
+    openModal(28);
+}
+
+function setHonda() {
+    openModal(29);
+}
+
+function setToyota() {
+    openModal(30);
+}
+
+function setMetris() {
+    openModal(31);
+}
 function openModal(x) {
     $("#myModal").css("display", "block");
     switch (x) {
@@ -100,8 +155,8 @@ function openModal(x) {
                 $('.name').attr("alt", "Ford Focus ST");
                 break;
         case 6: $('.img1').attr("src", "img/pageCars/economy/rio/rio1.png");
-                $('.img2').attr("src", "img/pageCars/economyrio/rio2.png");
-                $('.img3').attr("src", "img/pageCars/economyrio/rio3.png");
+                $('.img2').attr("src", "img/pageCars/economy/rio/rio2.png");
+                $('.img3').attr("src", "img/pageCars/economy/rio/rio3.png");
                 $('.img4').attr("src", "img/pageCars/economy/rio/rio4.jpg");
                 $('.name').attr("alt", "Kia Rio");
                 break;
@@ -123,15 +178,15 @@ function openModal(x) {
                 $('.img4').attr("src", "img/pageCars/luxury/bmw/bmw4.jpg");
                 $('.name').attr("alt", "Bmw X6");
                 break;
-        case 10: $('.img1').attr("src", "img/pageCars/luxury/bentley/bentley1.png");
-                $('.img2').attr("src", "img/pageCars/luxury/bentley/bentley2.png");
-                $('.img3').attr("src", "img/pageCars/luxury/bentley/bentley3.png");
+        case 10: $('.img1').attr("src", "img/pageCars/luxury/bentley/bentley1.jpg");
+                $('.img2').attr("src", "img/pageCars/luxury/bentley/bentley2.jpg");
+                $('.img3').attr("src", "img/pageCars/luxury/bentley/bentley3.jpg");
                 $('.img4').attr("src", "img/pageCars/luxury/bentley/bentley4.jpg");
                 $('.name').attr("alt", "Bentley Mulsanne");
                 break;
         case 11: $('.img1').attr("src", "img/pageCars/luxury/wraith/wraith1.png");
                 $('.img2').attr("src", "img/pageCars/luxury/wraith/wraith2.png");
-                $('.img3').attr("src", "img/pageCars/luxury/wraith/wraith3.png");
+                $('.img3').attr("src", "img/pageCars/luxury/wraith/wraith3.jpg");
                 $('.img4').attr("src", "img/pageCars/luxury/wraith/wraith4.jpg");
                 $('.name').attr("alt", "Rolls-Royce Wraith");
                 break;
@@ -170,6 +225,90 @@ function openModal(x) {
                 $('.img3').attr("src", "img/pageCars/luxury/tesla/tesla3.jpg");
                 $('.img4').attr("src", "img/pageCars/luxury/tesla/tesla4.jpg");
                 $('.name').attr("alt", "Tesla Model S");
+                break;
+        case 18: $('.img1').attr("src", "img/pageCars/sport/aventador/aventador1.png");
+                $('.img2').attr("src", "img/pageCars/sport/aventador/aventador2.png");
+                $('.img3').attr("src", "img/pageCars/sport/aventador/aventador3.png");
+                $('.img4').attr("src", "img/pageCars/sport/aventador/aventador4.jpg");
+                $('.name').attr("alt", "Lamborghini Aventador");
+                break;
+        case 19: $('.img1').attr("src", "img/pageCars/sport/mclaren/mclaren1.jpg");
+                $('.img2').attr("src", "img/pageCars/sport/mclaren/mclaren2.jpg");
+                $('.img3').attr("src", "img/pageCars/sport/mclaren/mclaren3.jpg");
+                $('.img4').attr("src", "img/pageCars/sport/mclaren/mclaren4.jpg");
+                $('.name').attr("alt", "McLaren 720S");
+                break;
+        case 20: $('.img1').attr("src", "img/pageCars/sport/aston/aston1.png");
+                $('.img2').attr("src", "img/pageCars/sport/aston/aston2.png");
+                $('.img3').attr("src", "img/pageCars/sport/aston/aston3.png");
+                $('.img4').attr("src", "img/pageCars/sport/aston/aston4.jpg");
+                $('.name').attr("alt", "Aston Martin DB11");
+                break;
+        case 21: $('.img1').attr("src", "img/pageCars/sport/chiron/chiron1.png");
+                $('.img2').attr("src", "img/pageCars/sport/chiron/chiron2.png");
+                $('.img3').attr("src", "img/pageCars/sport/chiron/chiron3.png");
+                $('.img4').attr("src", "img/pageCars/sport/chiron/chiron4.jpg");
+                $('.name').attr("alt", "Bugatti Chiron");
+                break;
+        case 22: $('.img1').attr("src", "img/pageCars/sport/viper/viper1.png");
+                $('.img2').attr("src", "img/pageCars/sport/viper/viper2.png");
+                $('.img3').attr("src", "img/pageCars/sport/viper/viper3.png");
+                $('.img4').attr("src", "img/pageCars/sport/viper/viper4.jpg");
+                $('.name').attr("alt", "Dodge Viper");
+                break;
+        case 23: $('.img1').attr("src", "img/pageCars/sport/nissan/nissan1.png");
+                $('.img2').attr("src", "img/pageCars/sport/nissan/nissan2.png");
+                $('.img3').attr("src", "img/pageCars/sport/nissan/nissan3.png");
+                $('.img4').attr("src", "img/pageCars/sport/nissan/nissan4.jpg");
+                $('.name').attr("alt", "Nissan GT-R");
+                break;
+        case 24: $('.img1').attr("src", "img/pageCars/sport/camaro/camaro1.png");
+                $('.img2').attr("src", "img/pageCars/sport/camaro/camaro2.png");
+                $('.img3').attr("src", "img/pageCars/sport/camaro/camaro3.png");
+                $('.img4').attr("src", "img/pageCars/sport/camaro/camaro4.jpg");
+                $('.name').attr("alt", "Chevrolet Camaro");
+                break;
+        case 25: $('.img1').attr("src", "img/pageCars/sport/mustang/mustang1.png");
+                $('.img2').attr("src", "img/pageCars/sport/mustang/mustang2.png");
+                $('.img3').attr("src", "img/pageCars/sport/mustang/mustang3.png");
+                $('.img4').attr("src", "img/pageCars/sport/mustang/mustang4.jpg");
+                $('.name').attr("alt", "Ford Mustang V6 5.0");
+                break;
+        case 26: $('.img1').attr("src", "img/pageCars/sport/alfa/alfa1.png");
+                $('.img2').attr("src", "img/pageCars/sport/alfa/alfa2.png");
+                $('.img3').attr("src", "img/pageCars/sport/alfa/alfa3.png");
+                $('.img4').attr("src", "img/pageCars/sport/alfa/alfa4.jpg");
+                $('.name').attr("alt", "Alfa Romeo 4C");
+                break;
+        case 27: $('.img1').attr("src", "img/pageCars/sport/gts/gts1.jpg");
+                $('.img2').attr("src", "img/pageCars/sport/gts/gts2.png");
+                $('.img3').attr("src", "img/pageCars/sport/gts/gts3.jpg");
+                $('.img4').attr("src", "img/pageCars/sport/gts/gts4.jpg");
+                $('.name').attr("alt", "Mercedes GTS");
+                break;
+        case 28: $('.img1').attr("src", "img/pageCars/sport/corvette/corvette1.png");
+                $('.img2').attr("src", "img/pageCars/sport/corvette/corvette2.png");
+                $('.img3').attr("src", "img/pageCars/sport/corvette/corvette3.png");
+                $('.img4').attr("src", "img/pageCars/sport/corvette/corvette4.jpg");
+                $('.name').attr("alt", "Chevrolet Corvette");
+                break;
+        case 29: $('.img1').attr("src", "img/pageCars/vans/honda/honda1.png");
+                $('.img2').attr("src", "img/pageCars/vans/honda/honda2.png");
+                $('.img3').attr("src", "img/pageCars/vans/honda/honda3.png");
+                $('.img4').attr("src", "img/pageCars/vans/honda/honda4.jpg");
+                $('.name').attr("alt", "Honda Odyssey");
+                break;
+        case 30: $('.img1').attr("src", "img/pageCars/vans/toyota/toyota1.png");
+                $('.img2').attr("src", "img/pageCars/vans/toyota/toyota2.png");
+                $('.img3').attr("src", "img/pageCars/vans/toyota/toyota3.png");
+                $('.img4').attr("src", "img/pageCars/vans/toyota/toyota4.jpg");
+                $('.name').attr("alt", "Toyota Sienna");
+                break;
+        case 31: $('.img1').attr("src", "img/pageCars/vans/metris/metris1.png");
+                $('.img2').attr("src", "img/pageCars/vans/metris/metris2.png");
+                $('.img3').attr("src", "img/pageCars/vans/metris/metris3.png");
+                $('.img4').attr("src", "img/pageCars/vans/metris/metris4.jpg");
+                $('.name').attr("alt", "Mercedes Metris");
                 break;
         default: break;
     }
