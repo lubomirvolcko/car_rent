d47ccbfce8f94747c928b3cc4d8390741ed84697
Martin Hercka <martin.hercka@akademiasovy.sk>
2018-03-11 16:25:34 +0100

    -added price calculator

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 
 
 
-
+$("#help").hide();
+$("#week").hide();
+$("#weekend").hide()
 
 
 
@@ -23,13 +25,17 @@ var today2 = new Date();
 var day2 = ("0" + today2.getDate()).slice(-2);
 var month2 = ("0" + (today2.getMonth() + 1)).slice(-2);
 var today2=today2.getFullYear()+1+"-"+(month2)+"-"+(day2) ;
-
+var today3= new Date();
+var time3 = today3.getHours() + ":" + today3.getMinutes()  
+var day3 = ("0" + today3.getDate()).slice(-2);
+var month3 = ("0" + (today3.getMonth() + 1)).slice(-2);
+var today3 = today3.getFullYear()+"-"+(month3)+"-"+(day3) ;
 
 
 var nulll =" ";
 
 $("#inputDate").attr("min",today);
-
+$("#inputDelDate").html(today3);
 $("#inputDelDate").attr("min",today);
 
 $("#inputDate").change(function(){
@@ -383,8 +389,200 @@ $("#send").hide();
     });
 
 
+  $("#dbcar").change(function(){
+      
+      $.ajax({
+
+    url: 'http://localhost:8080/rent/price/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getPrice',
+    contentType:'application/json',
+    success: getPrice,
+   type:'GET'
+   
+
+
+
+        
+     });   
+
+          $.ajax({
+
+    url: 'http://localhost:8080/rent/perweekprice/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getWeekPrice',
+    contentType:'application/json',
+    success: getWeekPrice,
+   type:'GET'
+   
+
+
+
+        
+     });   
+
+           $.ajax({
+
+    url: 'http://localhost:8080/rent/perweekendprice/'+$(this).val(),
+     
+    data:null,
+    contentType:'application/json',
+    crossDomain: true,
+    dataType: 'jsonp',
+    
+    jsonpCallback:'getWeekendPrice',
+    contentType:'application/json',
+    success: getWeekendPrice,
+   type:'GET'
+   
+
+
+
+        
+     });   
+ 
+ 
+
+
+ function getPrice(data)
+{    
+console.log(data);
+  var temp=data.price;
+    
+     $("#help").html(temp);
+     
+
+
+     }
+
+
+ function getWeekPrice(data)
+{    
+console.log(data);
+  var week=data.PerWeekPrice;
+    
+     $("#week").html(week);
+     
+
+
+     }
+
+
+ function getWeekendPrice(data)
+{    
+console.log(data);
+  var weekend=data.PerWeekendPrice;
+    
+     $("#weekend").html(weekend);
+     
+
+
+     }
+
+
+
+
+
+
+
+}); 
+
+
+$("#inputDelDate,#dbcar").change(function(){
+
+
+var date_diff_indays = function(date1) {
+   var date1= $("#inputDate").val()
+ var date2= $("#inputDelDate").val()
+
+dt1 = new Date(date1);
+dt2 = new Date(date2);
+
+
+return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+
+
+
+}
+
+var date1= $("#inputDate").val()
+dt1 = new Date(date1);
+var day = dt1.getDay();
+var isWeekend = (day == 6) || (day == 0);
+
+
+
+
+
+
+
+
+
+var perWeek = $("#week").html();
+var perDay = $("#help").html();
+
+switch(date_diff_indays())
+{
+case 1: if(isWeekend)
+{
+var weekendprice= $("#weekend").html();
+ $("#price").html(weekendprice+"€");
+}
+else{
+
+$("#price").html(perDay+"€");
+}
+break;
+case 2: perDay=parseInt(perDay)+ parseInt(perDay);
+$("#price").html(perDay+"€");
+console.log(date_diff_indays());
+break;
+case 3: perDay=parseInt(perDay) +parseInt(perDay) + parseInt(perDay);
+$("#price").html(perDay+"€");
+break;
+case 4: perDay=parseInt(perDay) +parseInt(perDay) + parseInt(perDay) + parseInt(perDay);
+$("#price").html(perDay+"€");
+break;
+case 5: perDay=parseInt(perDay) +parseInt(perDay) + parseInt(perDay) + parseInt(perDay) +  parseInt(perDay);
+$("#price").html(perDay+"€");
+break;
+case 6: perDay=parseInt(perDay) +parseInt(perDay) + parseInt(perDay) + parseInt(perDay) +  parseInt(perDay)+ parseInt(perDay);
+$("#price").html(perDay+"€");
+break;
+case 7: $("#price").html(perWeek+"€");
+break;
+
+
+
+
+
+}
+
+ if(date_diff_indays()>8)
+$("#price").html("On agreement!");
+
+
+
+}); 
+
+      
+    
+
+
 }); /* function ready */
  
+
+  
 
 function send()
 {
